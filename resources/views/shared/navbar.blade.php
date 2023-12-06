afc29f782fcca19b90721d5452d35f53e4154e97
dinhkhanh2104 <130718674+dinhkhanh2104@users.noreply.github.com>
2023-12-06 12:30:07 +0700

    add navbar feature

---
+++ resources/views/shared/navbar.blade.php
@@ -0,0 +1,212 @@
+<div class="nav">
+    <ul class="navList">
+        <li class="navListFirst">
+            <a href="{{ route('homePage') }}">Sportswear</a>
+        </li>
+
+        <li class="navListMiddle">
+            <div class="navNewProduct nav-subnav-item">
+                <a href="{{ route('product-list') }}/">Sản phẩm mới</a>
+                <ol class="subnavNewProduct subnav-blur">
+                    <!-- Nam -->
+                    <li>
+                        <ul>
+                            <li class="bold mb1">
+                                <a href="{{ route('product-list') }}">Nam</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất cả sản phẩm</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Giày</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Áo</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Quần</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- Nữ -->
+                    <li>
+                        <ul>
+                            <li class="bold mb1">
+                                <a href="{{ route('product-list') }}">Nữ</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất cả sản phẩm</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Giày</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Áo</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Quần</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- Trẻ em -->
+                    <li>
+                        <ul>
+                            <li class="bold mb1">
+                                <a href="{{ route('product-list') }}">Trẻ em</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất cả sản phẩm</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Giày</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Áo</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Quần</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ol>
+            </div>
+
+            <div class="navMen subnav-blur nav-subnav-item">
+                <a href="{{ route('product-list') }}/?type=1">Nam</a>
+                <ul class="subnavMen ">
+                    <li><a href=" {{ route('product-list') }} ">Tất cả sản phẩm</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Giày</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Áo</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Quần</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Tất</a></li>
+                </ul>
+            </div>
+
+            <div class="navWomen subnav-blur nav-subnav-item">
+                <a href="{{ route('product-list') }}">Nữ</a>
+                <ul class="subnavWomen ">
+                    <li><a href=" {{ route('product-list') }} ">Tất cả sản phẩm</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Giày</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Áo</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Quần</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Tất</a></li>
+                </ul>
+            </div>
+
+            <div class="navKids subnav-blur nav-subnav-item">
+                <a href="{{ route('product-list') }}">Trẻ em</a>
+                <ul class="subnavKids ">
+                    <li><a href=" {{ route('product-list') }} ">Tất cả sản phẩm</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Giày</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Áo</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Quần</a></li>
+                    <li><a href=" {{ route('product-list') }} ">Tất</a></li>
+                </ul>
+            </div>
+
+            <div class="navPromotion subnav-blur nav-subnav-item">
+                <a href="{{ route('product-list') }}">Khuyến mãi</a>
+                <ol class="subnavPromotion">
+                    <!-- Nam -->
+                    <li>
+                        <ul>
+                            <li class="bold mb1">
+                                <a href="{{ route('product-list') }}">Nam</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất cả sản phẩm</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Giày</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Áo</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Quần</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <!-- Nữ -->
+                    <li>
+                        <ul>
+                            <li class="bold mb1">
+                                <a href="{{ route('product-list') }}">Nữ</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất cả sản phẩm</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Giày</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Áo</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Quần</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <!-- Trẻ em -->
+                    <li>
+                        <ul>
+                            <li class="bold mb1">
+                                <a href="{{ route('product-list') }}">Trẻ em</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất cả sản phẩm</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Giày</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Áo</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Quần</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('product-list') }}">Tất</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ol>
+            </div>
+
+            <div class="navHelp subnav-blur">
+                <a href="{{ route('guide') }}">Hướng dẫn</a>
+            </div>
+
+            <div class="navIntroduce">
+                <a href="{{ route('guide') }}">Giới thiệu</a>
+            </div>
+
+        </li>
+
+        <li class="navListEnd">
+            <a href="{{ route('product-list') }}">
+                <i class="ti-search"></i>
+            </a>
+            <a href="{{ route('login') }}">Đăng nhập</a>
+            <a href="{{ route('register') }}">Đăng ký</a>
+
+        </li>
+    </ul>
+</div>
