a170336e6a05a6e8a9a6a0ab9e15dd8ba57aaa51
trungpd0911 <trungphan9dxd@gmail.com>
2023-12-06 12:03:20 +0700

    layout base

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>admin</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/base.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/admin/sidebar.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <div class="sidebar-container">
+        <header>
+            <div class="logo">
+                <a href="#">
+                    <img class="sidebar-icon" src="{{ asset('assets/svg/logo.svg') }}" alt="Logo">
+                </a>
+            </div>
+            <div class="search-box">
+                <div class="search-icon">
+                    <img class="sidebar-icon" src="{{ asset('assets/svg/search-normal.svg') }}">
+                </div>
+                <input id="search-box-input" type="text" placeholder="Tìm kiếm">
+            </div>
+            <div class="right">
+                <div class="notify">
+                    <i class="fa-solid fa-bell"></i>
+                </div>
+                <div class="admin-button">
+                    <i class="fa-regular fa-user"></i>
+                </div>
+            </div>
+        </header>
+        <main class="admin-layout">
+            <div class="sidebar">
+                <a href="{{ route('error') }}" class="sidebar-item">
+                    <div class="sidebar-item-container" id="account">
+                        <div class="sidebar-item-icon">
+                            <i class="fa-solid fa-user"></i>
+                        </div>
+                        <p>Dash Board</p>
+                    </div>
+                </a>
+                <a href="{{ route('error') }}" class="sidebar-item">
+                    <div class="sidebar-item-container" id="order">
+                        <div class="sidebar-item-icon">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </div>
+                        <p>Đơn hàng</p>
+                    </div>
+                </a>
+                <a href="{{ route('admin_product') }}" class="sidebar-item">
+                    <div class="sidebar-item-container" id="product">
+                        <div class="sidebar-item-icon">
+                            <i class="fa-solid fa-bag-shopping"></i>
+                        </div>
+                        <p>Sản phẩm</p>
+                    </div>
+                </a>
+                <a href="{{ route('admin_customer') }}" class="sidebar-item">
+                    <div class="sidebar-item-container" id="customer">
+                        <div class="sidebar-item-icon">
+                            <i class="fa-solid fa-person"></i>
+                        </div>
+                        <p>Khách hàng</p>
+                    </div>
+                </a>
+                <a href="{{ route('admin_category') }}" class="sidebar-item">
+                    <div class="sidebar-item-container" id="category">
+                        <div class="sidebar-item-icon">
+                            <i class="fa-solid fa-shapes"></i>
+                        </div>
+                        <p>Hạng mục</p>
+                    </div>
+                </a>
+                <a href="{{ route('admin_coupon') }}" class="sidebar-item">
+                    <div class="sidebar-item-container" id="discount">
+                        <div class="sidebar-item-icon">
+                            <i class="fa-solid fa-piggy-bank"></i>
+                        </div>
+                        <p>Giảm giá</p>
+                    </div>
+                </a>
+                <a href="{{ route('admin_report') }}" class="sidebar-item">
+                    <div class="sidebar-item-container" id="revenue">
+                        <div class="sidebar-item-icon">
+                            <i class="fa-solid fa-dollar-sign"></i>
+                        </div>
+                        <p>Doanh thu</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="content">
+                @yield('content')
+            </div>
+        </main>
+    </div>
+    <script src="https://kit.fontawesome.com/34f5218fc0.js" crossorigin="anonymous" defer></script>';
+    @yield('js')
+</body>
+
+</html>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/base.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/shared/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/shared/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/shared/scroll_top_btn.css') }}">
+    {{-- dung de load scroll to top btn --}}
+    <script src="https://kit.fontawesome.com/34f5218fc0.js" crossorigin="anonymous"></script>
+    @yield('css')
+</head>
+
+<body>
+    @include('shared.navbar')
+    @include('shared.scroll_top_btn')
+
+    @yield('content')
+
+    @include('shared.footer')
+    <script src="{{ asset('assets/js/shared/scroll_top_btn.js') }}"></script>
+    @yield('js')
+</body>
+
+</html>
